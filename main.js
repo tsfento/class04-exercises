a39a837d7fecd98d0e1e82629c053d048c466df4
Tyler Fenton <tsfento@gmail.com>
2023-09-06 10:32:19 -0500

    Re-numbered exercises, bugfixes and cleaning up.

--- main.js
+++ main.js
@@ -13,10 +13,16 @@ function exDisplay(num) {
 
 // Exercise 1
 function ex1Button() {
-    const inputName = document.getElementById('exercise1Name');
+    const inputName1 = document.getElementById('exercise1Name').value;
     const modalText = document.querySelector('div.ex1-modal-body');
     const modal = new bootstrap.Modal(document.getElementById('exercise1Modal'));
-    modalText.innerHTML = `<h3>Hello, ${inputName.value}!</h3>`;
+
+    if(inputName1 == '') {
+        modalText.innerHTML = '<h3>All right, then. Keep your secrets.</h3>';
+    } else {
+        modalText.innerHTML = `<h3>${inputName1}</h3>`;
+    }
+
     modal.show();
 }
 
@@ -25,6 +31,11 @@ function ex2Button() {
     let ex2Num1 = document.getElementById('exercise2Num1').value;
     let ex2Num2 = document.getElementById('exercise2Num2').value;
     let ex2Result = document.getElementById('exercise2Result');
+
+    if(ex2Num1 == '' || ex2Num2 == '') {
+        ex2Num1 = 0;
+        ex2Num2 = 0;
+    }
 
     ex2Result.innerText = `${ex2Num1} x ${ex2Num2} = ${ex2Num1 * ex2Num2}`;
 }
@@ -43,7 +54,11 @@ function ex3Button() {
     person.age = document.getElementById('exercise3Age').value;
     person.gender = document.getElementById('exercise3Gender').value;
 
-    ex3Result.textContent = `${person.name} is a ${person.gender.toLowerCase()} aged ${person.age}.`;
+    if(person.name == '' || person.age == '' || person.gender == '') {
+        ex3Result.textContent = 'Please fill all fields.';
+    } else {
+        ex3Result.textContent = `${person.name} is a ${person.gender.toLowerCase()} aged ${person.age}.`;
+    }
 }
 
 // Exercise 4
@@ -60,13 +75,22 @@ function ex4Button() {
     car.model = document.getElementById('exercise4Model').value;
     car.year = document.getElementById('exercise4Year').value;
 
-    ex4Result.textContent = `The car is a ${car.year} ${car.make} ${car.model}.`;
+    if(car.make == '' || car.model == '' || car.year == '') {
+        ex4Result.textContent = 'Please fill all fields.';
+    } else {
+        ex4Result.textContent = `The car is a ${car.year} ${car.make} ${car.model}.`;
+    }
 }
 
 // Exercise 5
 function ex5Button() {
     let ex5String = document.getElementById('exercise5String').value;
     let ex5Result = document.getElementById('exercise5Result');
+
+    if(ex5String == '') {
+        ex5String = 'Blank';
+    }
+
     let revString = ex5String.split('').reverse().join('');
 
     ex5Result.textContent = `${ex5String} reversed is ${revString}.`;
@@ -77,6 +101,10 @@ function ex6Button() {
     let ex6Rad = document.getElementById('exercise6Radius').value;
     let ex6Result = document.getElementById('exercise6Result');
     
+    if(ex6Rad == '') {
+        ex6Rad = 0;
+    }
+
     function calculateArea(radius) {
         return Math.PI * (radius * radius);
     }
@@ -90,8 +118,9 @@ function ex7Button() {
     const now = new Date();
     let amOrPm = now.getHours() >= 12 ? 'PM' : 'AM';
     let stdTime = now.getHours() > 12 ? now.getHours() - 12 : now.getHours();
+    let leadingZeroMin = now.getMinutes() < 10 ? `0${now.getMinutes()}` : now.getMinutes();
 
-    ex7Result.textContent = `It is currently ${stdTime.toString()}:${now.getMinutes().toString()} ${amOrPm} on ${(now.getMonth() + 1).toString()}/${now.getDate().toString()}/${now.getFullYear().toString()}.`;
+    ex7Result.textContent = `It is currently ${stdTime}:${leadingZeroMin} ${amOrPm} on ${(now.getMonth() + 1)}/${now.getDate()}/${now.getFullYear()}.`;
 }
 
 // Exercise 8
@@ -101,56 +130,65 @@ function ex8Button() {
     ex8Result.textContent = (Math.floor(Math.random() * 10) + 1).toFixed().toString();
 }
 
+// Exercise 9
+function ex9Button() {
+    let ex9Str = document.getElementById('exercise9String').value;
+    let ex9Result = document.getElementById('exercise9Result');
+
+    if(ex9Str == '') {
+        ex9Result.textContent = `I'm gonna need some participation.`;
+    } else {
+        ex9Result.textContent = `You entered: ${ex9Str}.`;
+    }
+    
+}
+
 // Exercise 10
 function ex10Button() {
-    let ex10Str = document.getElementById('exercise10String').value;
+    let ex10Name = document.getElementById('exercise10Name').value;
+    let ex10Age = document.getElementById('exercise10Age').value;
+    let ex10Job = document.getElementById('exercise10Occupation').value;
     let ex10Result = document.getElementById('exercise10Result');
 
-    ex10Result.textContent = `You entered: ${ex10Str}.`;
+    const person = {
+        name: ex10Name.toString(),
+        age: ex10Age,
+        occupation: ex10Job.toString().toLowerCase(),
+    }
+
+    if(person.name == '' || person.age == '' || person.occupation == '') {
+        ex10Result.textContent = 'Please fill all fields.';
+    } else {
+        ex10Result.textContent = `${person.name} is ${person.age} years old and is employed as a ${person.occupation}.`;
+    }
 }
 
 // Exercise 11
 function ex11Button() {
-    let ex11Name = document.getElementById('exercise11Name').value;
-    let ex11Age = document.getElementById('exercise11Age').value;
-    let ex11Job = document.getElementById('exercise11Occupation').value;
-    let ex11Result = document.getElementById('exercise11Result');
+    let ex11Key1 = document.getElementById('exercise11Key1').value;
+    let ex11Key2 = document.getElementById('exercise11Key2').value;
+    let ex11Key3 = document.getElementById('exercise11Key3').value;
+    let ex11Key4 = document.getElementById('exercise11Key4').value;
+    let ex11Val1 = document.getElementById('exercise11Value1').value;
+    let ex11Val2 = document.getElementById('exercise11Value2').value;
+    let ex11Val3 = document.getElementById('exercise11Value3').value;
+    let ex11Val4 = document.getElementById('exercise11Value4').value;
 
-    const person = {
-        name: ex11Name.toString(),
-        age: ex11Age,
-        occupation: ex11Job.toString().toLowerCase(),
-    }
-
-    ex11Result.textContent = `${person.name} is ${person.age} years old and is employed as a ${person.occupation}.`;
-}
-
-// Exercise 12
-function ex12Button() {
-    let ex12Key1 = document.getElementById('exercise12Key1').value;
-    let ex12Key2 = document.getElementById('exercise12Key2').value;
-    let ex12Key3 = document.getElementById('exercise12Key3').value;
-    let ex12Key4 = document.getElementById('exercise12Key4').value;
-    let ex12Val1 = document.getElementById('exercise12Value1').value;
-    let ex12Val2 = document.getElementById('exercise12Value2').value;
-    let ex12Val3 = document.getElementById('exercise12Value3').value;
-    let ex12Val4 = document.getElementById('exercise12Value4').value;
-
-    let ex12K1Result = document.getElementById('exercise12Key1Sorted');
-    let ex12K2Result = document.getElementById('exercise12Key2Sorted');
-    let ex12K3Result = document.getElementById('exercise12Key3Sorted');
-    let ex12K4Result = document.getElementById('exercise12Key4Sorted');
-    let ex12V1Result = document.getElementById('exercise12Value1Sorted');
-    let ex12V2Result = document.getElementById('exercise12Value2Sorted');
-    let ex12V3Result = document.getElementById('exercise12Value3Sorted');
-    let ex12V4Result = document.getElementById('exercise12Value4Sorted');
+    let ex11K1Result = document.getElementById('exercise11Key1Sorted');
+    let ex11K2Result = document.getElementById('exercise11Key2Sorted');
+    let ex11K3Result = document.getElementById('exercise11Key3Sorted');
+    let ex11K4Result = document.getElementById('exercise11Key4Sorted');
+    let ex11V1Result = document.getElementById('exercise11Value1Sorted');
+    let ex11V2Result = document.getElementById('exercise11Value2Sorted');
+    let ex11V3Result = document.getElementById('exercise11Value3Sorted');
+    let ex11V4Result = document.getElementById('exercise11Value4Sorted');
 
     let objToSort = {};
 
-    objToSort[ex12Key1.toString()] = ex12Val1.toString();
-    objToSort[ex12Key2.toString()] = ex12Val2.toString();
-    objToSort[ex12Key3.toString()] = ex12Val3.toString();
-    objToSort[ex12Key4.toString()] = ex12Val4.toString();
+    objToSort[ex11Key1.toString()] = ex11Val1.toString();
+    objToSort[ex11Key2.toString()] = ex11Val2.toString();
+    objToSort[ex11Key3.toString()] = ex11Val3.toString();
+    objToSort[ex11Key4.toString()] = ex11Val4.toString();
 
     let keyArr = Object.keys(objToSort).sort();
     let valArr = [];
@@ -159,35 +197,40 @@ function ex12Button() {
         valArr.push(objToSort[keyArr[i].toString()]);
     }
 
-    ex12K1Result.textContent = keyArr[0].toString();
-    ex12K2Result.textContent = keyArr[1].toString();
-    ex12K3Result.textContent = keyArr[2].toString();
-    ex12K4Result.textContent = keyArr[3].toString();
-    ex12V1Result.textContent = valArr[0].toString();
-    ex12V2Result.textContent = valArr[1].toString();
-    ex12V3Result.textContent = valArr[2].toString();
-    ex12V4Result.textContent = valArr[3].toString();
+    ex11K1Result.textContent = keyArr[0].toString();
+    ex11K2Result.textContent = keyArr[1].toString();
+    ex11K3Result.textContent = keyArr[2].toString();
+    ex11K4Result.textContent = keyArr[3].toString();
+    ex11V1Result.textContent = valArr[0].toString();
+    ex11V2Result.textContent = valArr[1].toString();
+    ex11V3Result.textContent = valArr[2].toString();
+    ex11V4Result.textContent = valArr[3].toString();
+}
+
+// Exercise 12
+function ex12Button() {
+    let ex12Sentence = document.getElementById('exercise12Sentence').value;
+    let ex12Result = document.getElementById('exercise12Result');
+
+    if(ex12Sentence == '') {
+        ex12Sentence = `I'm gonna need some participation.`;
+    }
+
+    let revSentence = ex12Sentence.split(' ').reverse().join(' ');
+
+    ex12Result.textContent = `${revSentence}`;
 }
 
 // Exercise 13
 function ex13Button() {
-    let ex13Sentence = document.getElementById('exercise13Sentence').value;
+    let ex13Parens = document.getElementById('exercise13Parens').value;
     let ex13Result = document.getElementById('exercise13Result');
-    let revSentence = ex13Sentence.split(' ').reverse().join(' ');
-
-    ex13Result.textContent = `${revSentence}`;
-}
-
-// Exercise 14
-function ex14Button() {
-    let ex14Parens = document.getElementById('exercise14Parens').value;
-    let ex14Result = document.getElementById('exercise14Result');
 
     const tempArr = [];
     const parens = '(){}[]';
     
-    for (let i = 0; i < ex14Parens.length; i++) {
-        tempArr.push(ex14Parens[i]);
+    for (let i = 0; i < ex13Parens.length; i++) {
+        tempArr.push(ex13Parens[i]);
 
         let oParen = tempArr[tempArr.length - 2];
         let cParen = tempArr[tempArr.length - 1];
@@ -200,108 +243,116 @@ function ex14Button() {
         }
     }
 
-    if(tempArr.length === 0) {
-        ex14Result.textContent = `${ex14Parens} is balanced.`;
-    } else {
-        ex14Result.textContent = `${ex14Parens} is not balanced`;
+    if(ex13Parens == '') {
+        ex13Result.textContent = `Nothing is balanced. As in all things. `;
+    } else if(tempArr.length === 0 && ex13Parens != '') {
+        ex13Result.textContent = `${ex13Parens} is balanced.`;
+    } else{
+        ex13Result.textContent = `${ex13Parens} is not balanced`;
+    }
+}
+
+// Exercise 14
+function ex14Button() {
+    let ex14Word1 = document.getElementById('exercise14Word1').value;
+    let ex14Word2 = document.getElementById('exercise14Word2').value;
+    let ex14Result = document.getElementById('exercise14Result');
+
+    let anaCheckArr = [];
+
+    if(ex14Word1.length == 0 || ex14Word2.length == 0) {
+        ex14Result.innerText = 'Please enter two words.';
+    } else if(ex14Word1.length != ex14Word2.length) {
+        ex14Result.innerText = `${ex14Word1} is not an anagram of ${ex14Word2}.`;
+    }
+
+    for (let i = 0; i < ex14Word2.length; i++) {
+        anaCheckArr.push(ex14Word1[i]);
+
+        if(ex14Word2.includes(anaCheckArr[0])) {
+            anaCheckArr.pop();
+        }
+    }
+
+    if(anaCheckArr.length === 0 && ex14Word1.length !== 0 && ex14Word2.length !== 0) {
+        ex14Result.innerText = `${ex14Word1} is an anagram of ${ex14Word2}.`;
+    } else if(ex14Word1.length !== 0 && ex14Word2.length !== 0) {
+        ex14Result.innerText = `${ex14Word1} is not an anagram of ${ex14Word2}.`;
     }
 }
 
 // Exercise 15
 function ex15Button() {
-    let ex15Word1 = document.getElementById('exercise15Word1').value;
-    let ex15Word2 = document.getElementById('exercise15Word2').value;
     let ex15Result = document.getElementById('exercise15Result');
-
-    let anaCheckArr = [];
-
-    if(ex15Word1.length == 0 || ex15Word2.length == 0) {
-        ex15Result.innerText = 'Please enter two words.';
-    } else if(ex15Word1.length != ex15Word2.length) {
-        ex15Result.innerText = `${ex15Word1} is not an anagram of ${ex15Word2}.`;
-    }
-
-    for (let i = 0; i < ex15Word2.length; i++) {
-        anaCheckArr.push(ex15Word1[i]);
-
-        if(ex15Word2.includes(anaCheckArr[0])) {
-            anaCheckArr.pop();
-        }
-    }
-
-    if(anaCheckArr.length === 0 && ex15Word1.length !== 0 && ex15Word2.length !== 0) {
-        ex15Result.innerText = `${ex15Word1} is an anagram of ${ex15Word2}.`;
-    } else if(ex15Word1.length !== 0 && ex15Word2.length !== 0) {
-        ex15Result.innerText = `${ex15Word1} is not an anagram of ${ex15Word2}.`;
-    }
-}
-
-// Exercise 16
-function ex16Button() {
-    let ex16Result = document.getElementById('exercise16Result');
-    let ex16FizzBuzzArr = [];
+    let ex15FizzBuzzArr = [];
     let parsedArr = '';
 
     for (let i = 1; i <= 100; i++) {
         if(i > 1 && i % 3 === 0 && i % 5 === 0) {
-            ex16FizzBuzzArr.push('FizzBuzz');
+            ex15FizzBuzzArr.push('FizzBuzz');
         } else if(i > 1 && i % 3 === 0) {
-            ex16FizzBuzzArr.push('Fizz');
+            ex15FizzBuzzArr.push('Fizz');
         } else if (i > 1 && i % 5 === 0) {
-            ex16FizzBuzzArr.push('Buzz');
+            ex15FizzBuzzArr.push('Buzz');
         } else {
-            ex16FizzBuzzArr.push(i);
+            ex15FizzBuzzArr.push(i);
         }
     }
 
-    for (let j = 0; j < ex16FizzBuzzArr.length; j++) {
-        parsedArr += `${ex16FizzBuzzArr[j]} `;
+    for (let j = 0; j < ex15FizzBuzzArr.length; j++) {
+        parsedArr += `${ex15FizzBuzzArr[j]} `;
     }
 
-    ex16Result.textContent = parsedArr;
+    ex15Result.textContent = parsedArr;
 }
 
-// Exercise 17
-function ex17Button() {
-    let ex17Word = document.getElementById('exercise17Word').value;
-    let ex17Result = document.getElementById('exercise17Result');
-    let checkStr1 = ex17Word.toLowerCase().split(' ').join('');
+// Exercise 16
+function ex16Button() {
+    let ex16Word = document.getElementById('exercise16Word').value;
+    let ex16Result = document.getElementById('exercise16Result');
+    let checkStr1 = ex16Word.toLowerCase().split(' ').join('');
     
     punctuationArr = [];
-    punctuation = `.,;:!?@#$%^&*()-+={}[]/<>'"`;
+    punctuation = `. , ; : ! ? @ # $ % ^ & * ( ) - + = { } [ ] / < > ' "`;
+
+    let prevI = 0;
 
     for(let i = 0; i < checkStr1.length; i++) {
         punctuationArr.push(checkStr1[i]);
 
-        if(punctuation.includes(punctuationArr[i])) {
+        if(punctuation.includes(punctuationArr[i - prevI])) {
             punctuationArr.pop();
+            prevI++;
         }
     }
 
     checkStr1 = punctuationArr.join('');
     let checkStr2 = checkStr1.split('').reverse().join('');
 
-    //return checkStr1 === checkStr2;
-    if(checkStr1 === checkStr2) {
-        ex17Result.textContent = `${checkStr1} is a palindrome.`;
+    if(ex16Word == '') {
+        ex16Result.textContent = `Please enter a word.`;
+    } else if(checkStr1 === checkStr2) {
+        ex16Result.textContent = `${checkStr1} is a palindrome.`;
     } else {
-        ex17Result.textContent = `${checkStr1} is not a palindrome.`;
+        ex16Result.textContent = `${checkStr1} is not a palindrome.`;
     }
 }
 
-// Exercise 18
-function ex18Button() {
-    let ex18Sentence = document.getElementById('exercise18Sentence').value;
-    let ex18Result = document.getElementById('exercise18Result');
+// Exercise 17
+function ex17Button() {
+    let ex17Sentence = document.getElementById('exercise17Sentence').value;
+    let ex17Result = document.getElementById('exercise17Result');
     
-    let splitSentence = ex18Sentence.split(" ");
+    if(ex17Sentence == '') {
+        ex17Sentence = `I'm gonna need some cooperation.`;
+    }
+
+    let splitSentence = ex17Sentence.split(" ");
     let joinSentence = [];
 
     for (let i = 0; i < splitSentence.length; i++) {
         joinSentence.push(splitSentence[i].split('').reverse().join(''));
     }
 
-    //return joinSentence.join(' ');
-
-    ex18Result.textContent = `${joinSentence.join(' ')}`;
+    ex17Result.textContent = `${joinSentence.join(' ')}`;
 }
